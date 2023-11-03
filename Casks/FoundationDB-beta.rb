d87cb027ac12b8fcbfacc0664089611651de4622
Sergey Grankin <sgrankin@gmail.com>
2023-11-02 20:25:29 -0400

    FoundationDB 7.3

--- Casks/FoundationDB-beta.rb
+++ Casks/FoundationDB-beta.rb
@@ -1,13 +1,13 @@
-cask "foundationdb" do
+cask "foundationdb-beta" do
   name "FoundationDB"
   desc "FoundationDB"
   homepage "https://www.foundationdb.org"
 
   arch arm: "arm64", intel: "x86_64"
 
-  version "7.1.43"
-  sha256 arm: "685c1087dcf5d552e59a8c24764c535e9281c1e4501e06c2a0103b8ea2c39b2b",
-         intel: "7c7e1784a84690cdecb629ba99f5232da1eaf29c473751178600c16b8b88e3dd"
+  version "7.3.25"
+  sha256 arm: "87a51f4f1aaa30402a899cecd71c8f22a76177e502f6ad485c561ae85d7d5fe7",
+         intel: "69795d4d3194c26c0d5a58f59a2bb6b854994ec24845f7f7f90f20232b5e09f3"
 
   url "https://github.com/apple/foundationdb/releases/download/#{version}/FoundationDB-#{version}_#{arch}.pkg",
     verified: "github.com/apple/foundationdb/"
@@ -16,7 +16,7 @@ cask "foundationdb" do
     strategy :github_latest
   end
 
-  conflicts_with cask: "foundationdb-beta"
+  conflicts_with cask: "foundationdb"
 
   pkg "FoundationDB-#{version}_#{arch}.pkg"
   uninstall pkgutil: [
