697a3bdd1d5e465b5ec7356b2e336f7b6023ad13
Sergey Grankin <sgrankin@gmail.com>
2023-11-08 11:15:25 -0500

    fdb 7.3.27

--- Casks/foundationdb-beta.rb
+++ Casks/foundationdb-beta.rb
@@ -1,9 +1,9 @@
 cask "foundationdb-beta" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "7.3.25"
-  sha256 arm:   "87a51f4f1aaa30402a899cecd71c8f22a76177e502f6ad485c561ae85d7d5fe7",
-         intel: "69795d4d3194c26c0d5a58f59a2bb6b854994ec24845f7f7f90f20232b5e09f3"
+  version "7.3.27"
+  sha256 arm:   "585103123cb8adb0f2813c47c1b12ec6757924b4a8880f00b1cd03d1ed2556fd",
+         intel: "57287c3deab1824f7f1bbc3728a4538d171da9d5a363975813bef07a8cf15653"
 
   url "https://github.com/apple/foundationdb/releases/download/#{version}/FoundationDB-#{version}_#{arch}.pkg",
       verified: "github.com/apple/foundationdb/"
